8bca841e2b6f72a0f21dca1db9638542e103f590
Kevin <kevin@chun.gd>
2012-04-24 12:36:53 -0700

    auto-detect .haml files

--- init.el
+++ init.el
@@ -19,6 +19,7 @@
 
 ;; Haml Mode
 (add-to-list 'load-path "~/.emacs.d/haml-mode")
+(add-to-list 'auto-mode-alist '("\\.haml\\'"   . haml-mode))
 (autoload 'haml-mode "haml-mode" "Mode for haml" t)
 
 ;; nginx-mode
