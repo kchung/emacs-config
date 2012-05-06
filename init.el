6207d2b05a70beda85b0d53e051cce978989966c
Kevin <kevin@chun.gd>
2012-05-06 14:02:14 -0700

    added ido mode

--- init.el
+++ init.el
@@ -8,6 +8,9 @@
 (autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
 
+;; InteractivelyDoThings (ido)
+(require 'ido)   
+
 ;; Magit 
 ;; http://philjackson.github.com/magit/
 (require 'magit)
